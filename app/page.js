8bb75d86b4b4d436fb47cbc057d79a815448eee4
ISAWASHUN <syun20000616@icloud.com>
2024-02-01 21:23:14 +0900

    componentの作成

--- app/page.js
+++ app/page.js
@@ -1,7 +1,20 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
+import Stats from "./components/Stats";
+import Services from "./components/Services";
+import Solution from "./components/Solution";
+import Testimonials from "./components/Testimonials";
+import Blog from "./components/Blog";
 
 export default function Home() {
   return (
-  <p></p>
+    <>
+      <Hero />
+      <Stats />
+      <Services/>
+      <Solution/>
+      <Testimonials/>
+      <Blog/>
+    </>
   );
 }
