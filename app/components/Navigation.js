43b3c119d3e70b8583f4a67a56778314079e23d5
ISAWASHUN <syun20000616@icloud.com>
2024-02-01 22:00:29 +0900

    Navigationの大枠の実装

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,8 +1,53 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { HiOutlineBars3 } from 'react-icons/hi2'
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <>
+      {/* webメニュー */}
+      <header>
+        <div>
+          <div>
+            {/* ロゴ */}
+            <div>
+              <Link href={"/"}>
+                <Image
+                src={'/assets/logo.png'}
+                alt="logo"
+                width={128}
+                height={128} />
+              </Link>
+            </div>
+
+            {/* メニュー */}
+            <div>
+              <ul>
+                <li>
+                  <Link href={"/"}>
+                    Home
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            {/* ボタン */}
+            <div>
+              <Link href={"/"}>
+                申し込み
+              </Link>
+
+              {/* モバイル */}
+              <button>
+                <HiOutlineBars3/>
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {/*  */}
+    </>
   )
 }
 
