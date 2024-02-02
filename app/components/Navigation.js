926cb7a0efc6a542796615957581e015798a7cea
ISAWASHUN <syun20000616@icloud.com>
2024-02-02 21:09:39 +0900

    Navigationのデータの実装

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -3,6 +3,29 @@ import Link from 'next/link'
 import React from 'react'
 import { HiOutlineBars3 } from 'react-icons/hi2'
 
+const navigationMenu = [
+  {
+    href: "#home",
+    label: "ホーム"
+  },
+  {
+    href: "#services",
+    label: "サービス"
+  },
+  {
+    href: "#solution",
+    label: "解決策"
+  },
+  {
+    href: "#testimonials",
+    label: "お客様の声"
+  },
+  {
+    href: "#blog",
+    label: "ブログ"
+  },
+]
+
 const Navigation = () => {
   return (
     <>
@@ -24,9 +47,15 @@ const Navigation = () => {
             {/* メニュー */}
             <div>
               <ul>
+                {navigationMenu.map((item, index) => (
+                  <li key={index}>
+                    <Link href={item.href}>
+                      {item.label}
+                    </Link>
+                  </li>
+                ))}
                 <li>
                   <Link href={"/"}>
-                    Home
                   </Link>
                 </li>
               </ul>
