9026d2242a3583c40fd9b6258ffcae15cd3141a1
ISAWASHUN <syun20000616@icloud.com>
2024-02-05 21:50:46 +0900

    Heroの大枠

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,8 +1,59 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section>
+      <div>
+        <div>
+          {/* Left */}
+          <div>
+            <span>サブタイトル</span>
+            <h1>タイトル</h1>
+            <p>詳細</p>
+            <div>
+              <Link href={''}>資料ダウンロード</Link>
+              <Link href={''}>申し込み</Link>
+            </div>
+          </div>
+
+          {/* right */}
+          <div>
+            {/* 上の段 */}
+            <div>
+              <div>
+                <Image src={''} alt={''} width={''} height={''} />
+              </div>
+              <div>
+                <div>
+                  <div>
+                    <Image src={''} alt={''} width={''} height={''} />
+                  </div>
+                  <div></div>
+                </div>
+                <div>
+                  <Image src={''} alt={''} width={''} height={''} />
+                </div>
+              </div>
+            </div>
+            {/* したの段 */}
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <Image src={''} alt={''} width={''} height={''} />
+                  </div>
+                </div>
+                <div>
+                  <Image src={''} alt={''} width={''} height={''} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
