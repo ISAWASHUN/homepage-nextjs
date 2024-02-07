9c9e285583a78600f428847314f3b80131a76875
ISAWASHUN <syun20000616@icloud.com>
2024-02-07 23:17:38 +0900

    Statsの大枠

--- app/components/Stats.js
+++ app/components/Stats.js
@@ -1,8 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { BiChevronRight } from 'react-icons/bi'
 
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <section>
+      <div>
+        <div>
+          <div>
+            <strong>99.9%</strong>
+            <span>現役合格率</span>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <Image src="" alt="icon online" width={64} height={64} />
+            <h3>タイトル</h3>
+            <p>詳細</p>
+            <Link href="/"><BiChevronRight/></Link>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
