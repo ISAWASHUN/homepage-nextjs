4ad3bbaa4281d44850a3a835c9e946f7cb8ae3ff
ISAWASHUN <syun20000616@icloud.com>
2024-02-07 23:21:40 +0900

    Statsのデータの作成

--- app/components/Stats.js
+++ app/components/Stats.js
@@ -3,22 +3,48 @@ import Link from 'next/link'
 import React from 'react'
 import { BiChevronRight } from 'react-icons/bi'
 
+const statsContent = {
+  stats: [
+    {
+      number : "99.9%",
+      label: "現役合格率",
+    },
+    {
+      number : "3912",
+      label: "生徒数",
+    },
+    {
+      number : "12.8",
+      label: "偏差値上昇",
+    },
+  ],
+  text: {
+    title: "担当アドバイザーによる徹底したサポート",
+    description: "担当アドバイザーによる徹底したサポートを受けることで、生徒の偏差値は平均12.8上昇しています。",
+    img: "/assets/images/illustration.svg",
+  }
+}
+
 const Stats = () => {
   return (
     <section>
       <div>
         <div>
           <div>
-            <strong>99.9%</strong>
-            <span>現役合格率</span>
+            {statsContent.stats.map((stat, index) => (
+              <div key={index}>
+                <strong>{stat.number}</strong>
+                <span>{stat.label}</span>
+              </div>
+            ))}
           </div>
         </div>
 
         <div>
           <div>
-            <Image src="" alt="icon online" width={64} height={64} />
-            <h3>タイトル</h3>
-            <p>詳細</p>
+            <Image src={statsContent.text.img} alt="icon online" width={100} height={200} />
+            <h3>{statsContent.text.title}</h3>
+            <p>{statsContent.text.description}</p>
             <Link href="/"><BiChevronRight/></Link>
           </div>
         </div>
