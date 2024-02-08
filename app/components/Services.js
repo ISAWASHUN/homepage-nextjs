b1f90763271f0b5cb782d4ef222d9bc19fb40ca1
ISAWASHUN <syun20000616@icloud.com>
2024-02-08 17:15:50 +0900

    ServicesのHTMLの大枠を実装

--- app/components/Services.js
+++ app/components/Services.js
@@ -1,8 +1,30 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section id='services'>
+      <div>
+        <div>
+          <span></span>
+          <h2></h2>
+          <p></p>
+        </div>
+        <div>
+          <div>
+            <div>
+              <span>
+                <Image src="/images/illustration-grow-together.svg" alt="Grow Together" width={100} height={100} />
+              </span>
+            </div>
+            <div>
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
