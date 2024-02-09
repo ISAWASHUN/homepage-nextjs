7950985c1dcae34a48802f1a64543eac94d04e85
ISAWASHUN <syun20000616@icloud.com>
2024-02-09 22:23:58 +0900

    Solutionsのデータの追加

--- app/components/Solutions.js
+++ app/components/Solutions.js
@@ -3,7 +3,37 @@ import Link from 'next/link';
 import React from 'react'
 import { FaCheck } from 'react-icons/fa';
 
-
+const solutionsContent = {
+  text: {
+    subTitle: '絶対合格を可能にする',
+    Title: '良質な教材・添削',
+    description:
+      '各大学の出題傾向を分析して作成した、ハイグレードな教室オリジナル教材を使用。選び抜かれた良問に取り組むことで、難関大で求められる「思考力」「表現力」を伸ばします',
+    features: [
+      {
+        title: '少人数制だからこそ可能な丁寧な指導',
+      },
+      {
+        title: '選べる学習スタイル',
+      },
+      {
+        title: '講師との距離が近く、質問しやすい',
+      },
+      {
+        title: '静かで集中できる自習室も完備',
+      },
+    ],
+  },
+  images: {
+    img1: '/assets/solution-img-1.jpg',
+    img2: '/assets/solution-img-2.jpg',
+    img3: '/assets/solution-img-3.jpg',
+  },
+  experience: {
+    year: '20年',
+    label: '支援されています',
+  },
+};
 
 const Solutions = () => {
   return (
